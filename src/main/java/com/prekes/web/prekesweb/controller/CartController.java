3ea1f0e0caed53505ef82d97b618bb87ff0a452b
joju6693 <jokubas.jurkys@mif.stud.vu.lt>
2022-04-25 22:55:25 +0300

    add: cart page

--- src/main/java/com/prekes/web/prekesweb/controller/CartController.java
+++ src/main/java/com/prekes/web/prekesweb/controller/CartController.java
@@ -5,11 +5,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ItemController {
+public class CartController {
 
-    @GetMapping("/item")
-    public String showItemPage(ModelMap model) {
-        System.out.println("Testing...");
-        return "item"; // view resolver /WEB-INF/jsp/welcome.jsp
+    @GetMapping("/cart")
+    public String showCartPage(ModelMap model) {
+        return "cart";
     }
 }
