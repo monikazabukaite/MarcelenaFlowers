721d83e160d14be74ee1114cc608f5207527756b
Jokūbas <jurkysjokubas@gmail.com>
2022-05-11 00:06:07 +0300

    add: primary implementation of interceptor logger added

---
+++ src/main/java/com/prekes/web/prekesweb/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.prekes.web.prekesweb;
+
+import com.prekes.web.prekesweb.interceptor.RequestLoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    RequestLoggingInterceptor requestLoggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggingInterceptor)
+                // NOTE: custom paths can be added here, you can filter out image resources requests etc.
+                .addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/prekes/web/prekesweb/interceptor/RequestLoggingInterceptor.java
@@ -0,0 +1,32 @@
+package com.prekes.web.prekesweb.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
+
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = Instant.now().toEpochMilli();
+        logger.info("Request URL::" + request.getRequestURL().toString() +
+                ":: Start Time=" + Instant.now());
+        request.setAttribute("startTime", startTime);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = (Long) request.getAttribute("startTime");
+        logger.info("Request URL::" + request.getRequestURL().toString() +
+                ":: Time Taken=" + (Instant.now().toEpochMilli() - startTime));
+    }
+}
