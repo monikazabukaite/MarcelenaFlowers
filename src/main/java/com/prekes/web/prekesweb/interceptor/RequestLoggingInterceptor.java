dd4e1b6d49145edd9c25c549c617d4ec98d8723b
Jokūbas <jurkysjokubas@gmail.com>
2022-05-12 19:20:42 +0300

    add: RequestLoggingInterceptor for user details implemented

--- src/main/java/com/prekes/web/prekesweb/interceptor/RequestLoggingInterceptor.java
+++ src/main/java/com/prekes/web/prekesweb/interceptor/RequestLoggingInterceptor.java
@@ -1,13 +1,17 @@
 package com.prekes.web.prekesweb.interceptor;
 
+import com.prekes.web.prekesweb.security.services.UserDetailsImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.Instant;
+import java.util.ArrayList;
 
 @Component
 public class RequestLoggingInterceptor implements HandlerInterceptor {
@@ -17,8 +21,6 @@ public class RequestLoggingInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         long startTime = Instant.now().toEpochMilli();
-        logger.info("Request URL::" + request.getRequestURL().toString() +
-                ":: Start Time=" + Instant.now());
         request.setAttribute("startTime", startTime);
         return true;
     }
@@ -26,7 +28,12 @@ public class RequestLoggingInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         long startTime = (Long) request.getAttribute("startTime");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = authentication.getPrincipal() instanceof UserDetailsImpl
+                ? (UserDetailsImpl) authentication.getPrincipal()
+                : new UserDetailsImpl(0L, "Guest", null, null, new ArrayList<>());
         logger.info("Request URL::" + request.getRequestURL().toString() +
-                ":: Time Taken=" + (Instant.now().toEpochMilli() - startTime));
+                ":: Time Taken = " + (Instant.now().toEpochMilli() - startTime) + "ms " +
+                ":: User details " + userDetails.toString());
     }
 }
